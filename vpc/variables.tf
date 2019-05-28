e1604ac8f8ab59367df30795163bacbe9f85082c
ahirtz <antoine.hirtz@gmail.com>
2019-05-28 17:22:17 +0200

    [TF] Change VPC CIDR

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -5,12 +5,12 @@ variable "aws_region" {
 
 variable "vpc_cidr" {
   type    = "string"
-  default = "172.23.0.0/16"
+  default = "10.1.0.0/16"
 }
 
 variable "subnet_cidrs_private" {
   type        = "list"
-  default     = ["172.23.1.0/24", "172.23.2.0/24"]
+  default     = ["10.1.1.0/24", "10.1.10.0/24"]
   description = "Subnet CIDRs for private subnets (length must match configured availability_zones)"
 
   # this could be further simplified / computed using cidrsubnet() etc.
