5ca0e738589c26dd6b45fb288c805bb6e1443814
ahirtz <antoine.hirtz@gmail.com>
2019-05-28 16:18:47 +0200

    [TF] Output subnets ids

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,3 +2,8 @@ output "aws_vpc_main_id" {
   value       = "${aws_vpc.main.id}"
   description = "The id of the main VPC"
 }
+
+output "aws_subnets_ids" {
+  value       = "${aws_subnet.subnets.*.id}"
+  description = "The ids of the private subnets"
+}
